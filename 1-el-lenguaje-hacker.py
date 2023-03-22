e9ae9744db9dc1711a10f47877a03a3e68a4ea7a
Mateo Gonzalez Marulanda <teogonmar97@gmail.com>
2023-03-22 07:40:55 -0500

    Starting challenge 1

--- 1-el-lenguaje-hacker.py
+++ 1-el-lenguaje-hacker.py
@@ -3,8 +3,58 @@
 # Escribe un programa que reciba un texto y transforme lenguaje natural a
 # "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
 #  se caracteriza por sustituir caracteres alfanuméricos.
-# - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+# - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/)
 #   con el alfabeto y los números en "leet".
 #   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
 
 #Desarrollo:
+text = str(input('Escribe el texto a continuacion: '))
+regular_text = text.capitalize()
+
+hacker_letters ={
+    "A":"4",
+    "B":"I3",
+    "C":"[",
+    "D":")",
+    "E":"3",
+    "F":"|=",
+    "G":"&",
+    "H":"#",
+    "I":"1",
+    "J":",_|",
+    "K":">|",
+    "L":"1",
+    "M":"JVI",
+    "N":"^/",
+    "O":"0",
+    "P":"|*",
+    "Q":"(_,)",
+    "R":"I2",
+    "S":"5",
+    "T":"7",
+    "U":"(_)",
+    "V":"|/",
+    "W":"\/\/",
+    "X":"><",
+    "Y":"j",
+    "Z":"2",
+    "1":"L",
+    "2":"R",
+    "3":"E",
+    "4":"A",
+    "5":"S",
+    "6":"b",
+    "7":"T",
+    "8":"B",
+    "9":"g",
+    "0":"o",
+}
+
+hacker_text = ""
+
+for i in range(len(regular_text)):
+    regular_word = regular_text[i]
+    hacker_word = regular_word.replace(regular_word, hacker_letters[regular_word])
+    hacker_text += hacker_word
+
+print(hacker_text)
